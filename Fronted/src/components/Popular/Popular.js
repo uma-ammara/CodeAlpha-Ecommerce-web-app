9e11b138b2215e6dda66ebe8fe7ee34f144d0a2f
uma-ammara <umaammaraa@gmail.com>
2026-01-05 20:12:26 +0500

    Create Popular.js

---
+++ Fronted/src/components/Popular/Popular.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+import './Popular.css'
+import Item from '../Item/item'
+
+const Popular = () => {
+  const [popularProducts, setPopularProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/popularinwomen')
+      .then((response) => response.json())
+      .then((data) => setPopularProducts(data))
+      .catch((err) => console.error("Fetch error:", err));
+  }, []);
+
+  return (
+    <div className='popular'>
+      <h1>POPULAR IN WOMEN</h1>
+      <hr />
+      <div className="popular-item">
+        {popularProducts.length > 0 ? (
+          popularProducts.map((item) => (
+            <Item
+              key={item.id}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              new_price={item.new_price}
+              old_price={item.old_price}
+            />
+          ))
+        ) : (
+          <p>No products found.</p>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default Popular
