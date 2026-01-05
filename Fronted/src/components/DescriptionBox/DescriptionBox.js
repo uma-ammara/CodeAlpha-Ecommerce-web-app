31a2fcbd8a3e3284f1c8b87b852ce296583dbf28
uma-ammara <umaammaraa@gmail.com>
2026-01-05 17:12:29 +0500

    Create DescriptionBox.js

---
+++ Fronted/src/components/DescriptionBox/DescriptionBox.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="description-navigator">
+            <div className="description-nav-box">Description</div>
+            <div className="description-nav-box fade">Reviews(122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>An e-commerce website is a platform where buyers and sellers can interact and conduct transactions online. It typically features a catalog of products or services, a shopping cart, and a secure payment gateway to facilitate purchases.An e-commerce website is a platform where buyers and sellers can interact and conduct transactions online. It typically features a catalog of products or services, a shopping cart, and a secure payment gateway to facilitate purchases.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem ducimus laudantium sapiente eveniet laboriosam nulla temporibus cum nam quasi voluptatum minima cupiditate sit, iste, modi voluptatem, placeat ea quas vero?</p>
+        </div>
+    </div>
+  )
+}
+
+
+export default DescriptionBox
